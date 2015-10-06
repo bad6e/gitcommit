bd6eab323ed9b0068f18c64e83f0de2282aa2985
Bret Doucette <doucette.bret@gmail.com>
2015-10-06 17:45:30 -0600

    Add names

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,12 +4,27 @@ class WelcomeController < ApplicationController
     urls = ["https://github.com/bad6e",
             "https://github.com/roseak",
             "https://github.com/mcschatz",
-            "https://github.com/travishaby"]
+            "https://github.com/travishaby",
+            "https://github.com/Jpease1020",
+            "https://github.com/jphoenix86",
+            "https://github.com/HoffsMH",
+            "https://github.com/MowAlon",
+            "https://github.com/russelleh",
+            "https://github.com/jbrr",
+            "https://github.com/rasensio1",
+            "https://github.com/selfup",
+            "https://github.com/dastinnette"
+          ]
 
-    @text = urls.map do |url|
-      Nokogiri::HTML(open(url))
+    text = urls.map do |url|
+      Nokogiri::HTML(open(url)).at_css(".vcard-fullname").text.strip
     end
 
+    text1 = urls.map do |url|
+      Nokogiri::HTML(open(url)).at_css(".contrib-number").text.gsub("total","").to_i
+    end
 
+    join = text.zip(text1)
+    @group = join.sort{|a,b| b[1] <=> a[1]}
   end
 end
--- app/models/welcome.rb
+++ app/models/welcome.rb
@@ -4,7 +4,17 @@ class Welcome
     urls = ["https://github.com/bad6e",
             "https://github.com/roseak",
             "https://github.com/mcschatz",
-            "https://github.com/travishaby"]
+            "https://github.com/travishaby",
+            "https://github.com/Jpease1020",
+            "https://github.com/jphoenix86",
+            "https://github.com/HoffsMH",
+            "https://github.com/MowAlon",
+            "https://github.com/russelleh",
+            "https://github.com/jbrr",
+            "https://github.com/rasensio1",
+            "https://github.com/selfup",
+            "https://github.com/dastinnette"
+          ]
 
     text = urls.map do |url|
       Nokogiri::HTML(open(url)).at_css
