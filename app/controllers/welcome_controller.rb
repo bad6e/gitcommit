b13fd23542bfeb0378ad88a5e82ab3a6c4d5169c
Bret Doucette <doucette.bret@gmail.com>
2015-10-06 16:10:24 -0600

    Added nokogiri

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    url = "https://github.com/bad6e"
+    data = Nokogiri::HTML(open(url))
+    @name  = data.at_css(".vcard-fullname").text.strip
+    @count = data.at_css(".contrib-number").text.strip
   end
 end
