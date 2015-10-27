4b6f02c90270334d1f6f8610c9866d5dfb07781e
Bret Doucette <doucette.bret@gmail.com>
2015-10-26 21:49:14 -0600

    Added commit messages

--- app/presenters/github_presenter.rb
+++ app/presenters/github_presenter.rb
@@ -27,12 +27,21 @@ class GithubPresenter
 
   def organizations
     list = service.find_user_organizations(user).map {|data| build_object(data)}
-    organizations = list.map {|organization| organization}
+    organizations = list.map {|organization| organization.login }
     organizations
   end
 
   def year_commits
     list = service.find_user_total_commits(user).map {|data| build_object(data)}
+  end
+
+  def starred_repos
+    list = service.total_starred_repos(user).map {|data| build_object(data)}
+    list.count
+  end
+
+  def commit_messages
+    list = service.commit_message(user)
   end
 
   private
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -26,6 +26,17 @@ class GithubService
     parse(connection.get("repos/#{user.nickname}/gitcommit/stats/commit_activity"))
   end
 
+  def total_starred_repos(user)
+    parse(connection.get("users/#{user.nickname}/starred"))
+  end
+
+  def commit_message(user)
+    list      = parse(connection.get("/users/#{user.nickname}/events"))
+    events    = list.select { |item| item[:type] == "PushEvent" }
+    commits   = events.map{ |event| event[:payload][:commits] }.flatten!
+    messages  = commits.collect{ |commit|  commit[:message] }
+  end
+
   def parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
