fa5f15fa169872ceb60e497423c44cb51a07ab5f
Bret Doucette <doucette.bret@gmail.com>
2015-10-28 15:38:43 -0600

    Updated views for stats to partials

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def show
     @current_user
     @user_info  ||= UserPresenter.new(current_user)
+    @stats      ||= StatsPresenter.new
   end
 end
