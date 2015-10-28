038b4989cca28b58050b786169de7aee7f000c21
Bret Doucette <doucette.bret@gmail.com>
2015-10-28 09:32:36 -0600

    Updated user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,6 @@
 class DashboardController < ApplicationController
   def show
-    if current_user == nil
-      current_user
-    else
-      @current_user if current_user
-      @user_info  ||= UserPresenter.new(current_user)
-    end
+    @current_user
+    @user_info  ||= UserPresenter.new(current_user)
   end
 end
