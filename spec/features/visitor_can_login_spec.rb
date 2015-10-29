d14ae52a639090200019d1204a238816ddfdf55f
Bret Doucette <doucette.bret@gmail.com>
2015-10-28 19:58:00 -0600

    Added VCR tests for Github API and methods for null data

--- spec/features/visitor_can_login_spec.rb
+++ spec/features/visitor_can_login_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
 feature "visitor can log in" do
+  before do
+    GithubPresenter.stub_chain(:new,:repos,:starred_repos).and_return("anything")
+    GithubPresenter.stub_chain(:new,:starred_repos).and_return("anything")
+    GithubPresenter.stub_chain(:new,:organizations).and_return("anything")
+    GithubPresenter.stub_chain(:new,:commit).and_return("anything")
+    GithubPresenter.stub_chain(:new,:followers).and_return("anything")
+    GithubPresenter.stub_chain(:new,:followees).and_return("anything")
+    GithubPresenter.stub_chain(:new,:year_commits).and_return("anything")
+    GithubPresenter.stub_chain(:new,:current_streaks).and_return("anything")
+    GithubPresenter.stub_chain(:new,:longest_streaks).and_return("anything")
+    GithubPresenter.stub_chain(:new,:followers_activities).and_return("anything")
+  end
 
   scenario "visitor can log in with oauth" do
     visit root_path
