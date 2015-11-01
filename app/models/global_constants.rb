59fec6c5ec1cab624c705548341365246d7d3771
Bret Doucette <doucette.bret@gmail.com>
2015-11-01 14:20:31 -0700

    temp update for users

--- app/models/global_constants.rb
+++ app/models/global_constants.rb
@@ -1,30 +1,12 @@
 module GlobalConstants
-  URLS = ["https://github.com/bad6e",
-            "https://github.com/roseak",
-            "https://github.com/mcschatz",
-            "https://github.com/travishaby",
-            "https://github.com/Jpease1020",
-            "https://github.com/jphoenix86",
-            "https://github.com/HoffsMH",
-            "https://github.com/MowAlon",
-            "https://github.com/russelleh",
-            "https://github.com/jbrr",
-            "https://github.com/rasensio1",
-            "https://github.com/selfup",
-            "https://github.com/dastinnette",
-            "https://github.com/mbburch",
-            "https://github.com/Kealii",
-            "https://github.com/Unsafepond",
-            "https://github.com/ChrisCenatie",
-            "https://github.com/adamki",
-            "https://github.com/plato721",
-            "https://github.com/Egogre"]
+  URLS = ["https://github.com/bad6e"]
 
   USERS = %w(
               bad6e
-              roseak
-              mcschatz
-              travishaby
+            )
+
+  USERS_ALL = %w(
+                  travishaby
               Jpease1020
               jphoenix86
               HoffsMH
@@ -42,5 +24,5 @@ module GlobalConstants
               plato721
               Egogre
               mikedao
-            )
+              )
 end
